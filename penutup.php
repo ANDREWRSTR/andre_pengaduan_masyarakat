43c2062779f1992c28d539b4b0e85aa9bf5b402c
ANDREWRSTR <140822784+ANDREWRSTR@users.noreply.github.com>
2023-08-09 11:38:34 +0700

    Add files via upload

---
+++ penutup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            margin: 0;
+        }
+        .user{
+            margin-right: 10px;
+            margin-left: auto;
+        }
+        header{
+            background-color:black;
+            color: #fff;
+            padding: 10px;
+            height:80px;
+        }
+        nav{
+           display: flex;
+        }
+        .user button{
+           margin-top:10px ;
+           padding: 8px 15px;
+           background-color: grey;
+           border: 1px solid white;
+           border-radius: 20px;
+           cursor:pointer;
+           color:#fff;
+        }
+    </style>
+</head>
+<body>
+<header>
+        <nav>
+            <div class="user">
+               <a link href=""> <button>MASUK</button></a>
+            <a link href=""><button>DAFTAR</button></a>
+            </div><br>
+        </nav>
+    </header>
+</body>
+</html>
